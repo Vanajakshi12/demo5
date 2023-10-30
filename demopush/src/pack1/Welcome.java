e45445d8a3e1a39c187bd745ade2afe3940f3e6b
Vanajakshi12 <mallelavanajakshi@gmail.com>
2023-10-30 11:58:08 +0530

    Commit 2

--- demopush/src/pack1/Welcome.java
+++ demopush/src/pack1/Welcome.java
@@ -5,5 +5,6 @@ public class Welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Welcome to GPREC");
+        System.out.println("Welcome to GIT");
 	}
 }
