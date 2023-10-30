508f0d92f22ad7a788bbfcf2cb3bc4886ab5d613
Vanajakshi12 <mallelavanajakshi@gmail.com>
2023-10-30 11:47:38 +0530

    Addition of two

---
+++ demopush/src/pack1/Add.java
@@ -0,0 +1,12 @@
+package pack1;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a, b, c;
+        c=a + b;
+        System.out.println(c);
+	}
+
+}
